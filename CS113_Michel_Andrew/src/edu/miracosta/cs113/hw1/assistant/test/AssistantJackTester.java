1cad292eed90debf912eec078586fb854ce8249e
Andrew Michel <drewmichel33@gmail.com>
2017-01-30 11:50:07 -0800

    Cleaning up

---
+++ CS113_Michel_Andrew/src/edu/miracosta/cs113/hw1/assistant/test/AssistantJackTester.java
@@ -0,0 +1,23 @@
+package edu.miracosta.cs113.hw1.assistant.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import edu.miracosta.cs113.hw1.assistant.AssistantJack;
+
+public class AssistantJackTester {
+
+    @Test
+    public void testCheckAnswerIntIntInt() {
+        AssistantJack aj = new AssistantJack(1);
+        assertTrue(aj.checkAnswer(1, 1, 1) == 0);       
+    }
+
+    @Test
+    public void testCheckAnswer2() {
+        AssistantJack aj = new AssistantJack(2);
+        assertTrue(aj.checkAnswer(6,10,6) == 0);
+    }
+
+}
