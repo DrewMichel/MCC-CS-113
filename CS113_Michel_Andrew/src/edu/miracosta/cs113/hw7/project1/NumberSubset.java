6ac4dde08e47f5791ecb6046cec4b6903dc8266a
Andrew Michel <drewmichel33@gmail.com>
2017-03-27 02:06:53 -0700

    Updating lecture7 lab2

---
+++ CS113_Michel_Andrew/src/edu/miracosta/cs113/hw7/project1/NumberSubset.java
@@ -0,0 +1,41 @@
+package edu.miracosta.cs113.hw7.project1;
+
+/**
+ * Created by Andrew Michel on 3/25/2017.
+ */
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class NumberSubset
+{
+    // main start
+    public static void main(String[] args)
+    {
+        subsetWrapper();
+    }// main end
+
+    // wrapper for recursion
+    public static void subsetWrapper()
+    {
+        // subset recursion
+        subsetRecursion();
+        // subsetDisplay
+        subsetDisplay();
+    }
+    // recursion
+    public static void subsetRecursion()
+    {
+        // base case
+
+
+        // subset recursion
+        // subsetRecursion()
+    }
+
+    // display for recursion
+    public static void subsetDisplay()
+    {
+        System.out.println("STUB IN subsetDisplay");
+    }
+}
